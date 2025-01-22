6b479220a054921fe9bd9af71911f93be02d2d19
Thea Bautista <thea.bautista@softwire.com>
2025-01-22 11:11:59 +0000

    M13: add logging

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -34,6 +34,8 @@ def process_orders(app):
             json=payload
         )
 
+        app.logger.info("Response from endpoint: " + response.text)
+
         response.raise_for_status()
 
         order.set_as_processed()
